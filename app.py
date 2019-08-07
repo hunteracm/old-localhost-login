7520de11ddb85a31de2835b2aa17db58c8c97d68
Khinshan Khan <khanshan501@gmail.com>
2019-08-07 12:48:22 -0400

    added in sql alchemy for db and slightly improved form

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+import os
+
 
 app = Flask(__name__)
 
+# database name based off of date name
+now = datetime.datetime.now()
+f = "data/{}.db".format(now.strftime("%Y-%m-%d"))
 
+# configure database path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///{}".format(f)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# database setup
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+
+
+def add_to_db(data):
+    # db.session.add(User(fname="Example", lname="example", email="example@example.com"))
+    # db.session.commit()
+    print(data)
+    return
+
+
+# routes begin here
 @app.route('/')
 def root():
     return render_template('index.html')
@@ -10,11 +39,11 @@ def root():
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    result = request.form
-    # add to database after mini parsing
+    add_to_db(request.form)
     return redirect(url_for('root'))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.debug = True
     app.run()
